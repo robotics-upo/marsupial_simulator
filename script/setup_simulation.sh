31870994a252aeed0e75abaa7cbbf0b55b7f0817
david-alejo <churr.te@gmail.com>
2023-02-23 14:53:03 +0100

    Added -y option

--- script/setup_simulation.sh
+++ script/setup_simulation.sh
@@ -7,13 +7,13 @@ read -p "Installation PACKAGES Dependencies for Marsupial Simulator. Press a ENT
 
 # 1a. necessary packages
 sudo apt-get update
-sudo apt-get install lsb-release wget gnupg
+sudo apt-get install lsb-release wget gnupg -y
 
 # 2a. Install IGN fortress
 sudo wget https://packages.osrfoundation.org/gazebo.gpg -O /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
 sudo apt-get update
-sudo apt-get install ignition-fortress
+sudo apt-get install ignition-fortress -y
 
 
 # Install ROS-IGN bridge (noetic)
