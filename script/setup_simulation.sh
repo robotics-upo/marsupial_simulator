4419c2ea21031471fa1f498f9fbf8a263a8fbd45
david-alejo <churr.te@gmail.com>
2023-02-23 15:44:41 +0100

    Added joy

--- script/setup_simulation.sh
+++ script/setup_simulation.sh
@@ -14,6 +14,7 @@ sudo wget https://packages.osrfoundation.org/gazebo.gpg -O /usr/share/keyrings/p
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
 sudo apt-get update
 sudo apt-get install ignition-fortress -y
+sudo apt-get install ros-noetic-joy
 
 
 # Install ROS-IGN bridge (noetic)
