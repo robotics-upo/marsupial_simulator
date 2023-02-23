383d14901ee791e6a9c628d2eb861aabfef46f11
david-alejo <churr.te@gmail.com>
2023-02-23 11:58:23 +0100

    Added ROS-IGN bridge

--- script/setup_simulation.sh
+++ script/setup_simulation.sh
@@ -3,20 +3,24 @@
 read -p "Installation PACKAGES Dependencies for Marsupial Simulator. Press a ENTER to contine. (CTRL+C) to cancel"
 
 
-# For drone simulation
-echo "\n Installing rotors_simulator \n\n"
-git clone -b https://github.com/ethz-asl/rotors_simulator.git
-echo "\n Installing mav_comm \n\n"
-git clone https://github.com/ethz-asl/mav_comm.git
+# 1. Ignition gazebo fortress (source https://gazebosim.org/docs/fortress/install_ubuntu)
 
-# For localization stuff
-echo "\n Installing DLL \n\n"
-git clone -noetic https://github.com/robotics-upo/dll.git
-echo "\n Installing odom to tf \n\n"
-git clone https://github.com/robotics-upo/odom_to_tf.git
+# 1a. necessary packages
+sudo apt-get update
+sudo apt-get install lsb-release wget gnupg
 
-# For Tracking
-echo "\n Installing matrice_traj_tracker \n\n"
-git clone https://github.com/robotics-upo/matrice_traj_tracker.git
-echo "\n Installing Onboard-SDK-ROS \n\n"
-git clone -b 3.8 https://github.com/dji-sdk/Onboard-SDK-ROS.git
+# 2a. Install IGN fortress
+sudo wget https://packages.osrfoundation.org/gazebo.gpg -O /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+sudo apt-get update
+sudo apt-get install ignition-fortress
+
+
+# Install ROS-IGN bridge (noetic)
+cd ~/marsupial_ws/src      # We assume that the catkin ws is in that folder
+git clone -b noetic https://github.com/gazebosim/ros_gz.git
+export IGNITION_VERSION=fortress
+cd ..
+source devel/setup.bash
+catkin_make
+
